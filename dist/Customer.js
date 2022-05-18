8c01bfb124637102f1545c59fd05ae889e9ff0bc
leofontolan <leofontolan@gmail.com>
2022-05-18 20:00:56 -0300

    Entidade Anemica

---
+++ dist/Customer.js
@@ -0,0 +1,23 @@
+"use strict";
+class Customer {
+    constructor(id, name, address) {
+        this._id = id;
+        this._name = name;
+        this._address = address;
+    }
+    get id() {
+        return this._id;
+    }
+    get name() {
+        return this._name;
+    }
+    get address() {
+        return this._address;
+    }
+    set name(name) {
+        this._name = name;
+    }
+    set address(address) {
+        this._address = address;
+    }
+}
