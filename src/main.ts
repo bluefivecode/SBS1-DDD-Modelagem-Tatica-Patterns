a1dadc2783dbab7bf6dc666d5637c8c24afd2731
leofontolan <leofontolan@gmail.com>
2022-05-20 19:23:54 -0300

    Criando uma Ordem de Serviço

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import Customer from "./entity/customer";
 import Address from "./entity/address";
+import OrderItem from "./entity/order_item";
+import Order from "./entity/order";
 
 let customer = new Customer("123", "Leo Fontolan");
 const address = new Address("Rua Dois", 2, "12345-678", "São Paulo");
 
 customer.address = address;
 customer.activate();
+
+const item1 = new OrderItem("1", "Item 1", 10);
+const item2 = new OrderItem("2", "Item 2", 15);
+
+const order = new Order("1", "123", [item1, item2]);
