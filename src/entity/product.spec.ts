9b2ad0c492a488c1e8f3016447451323cc26d53b
leofontolan <leofontolan@gmail.com>
2022-05-21 21:36:11 -0300

    Testando Mudando Preço e Nome

--- src/entity/product.spec.ts
+++ src/entity/product.spec.ts
@@ -10,6 +10,31 @@ describe("Product unit tests", () => {
         }).toThrowError("id is required");
 
     });
+
+    it("should throw error when name is empty", () => {
+
+        expect(() => {
+            const product = new Product("123", "", 100);
+        }).toThrowError("name is required");
+    });
+
+    it("should throw error when price is less than zero", () => {
+
+        expect(() => {
+            const product = new Product("123", "Product 1", -1);
+        }).toThrowError("price must be greater than zero");
+    });
     
+    it("should change name", () => {
+        const product = new Product("123", "Product 1", 100);
+        product.changeName("Product 2");
+        expect(product.name).toBe("Product 2");
+    });
+
+    it("should change price", () => {
+        const product = new Product("123", "Product 1", 100);
+        product.changePrice(150);
+        expect(product.price).toBe(150);
+    });
     
 });
--- src/entity/product.ts
+++ src/entity/product.ts
@@ -11,9 +11,35 @@ export default class{
         this.validate();
     }
 
+    changeName(name: string): void{
+        this._name = name;
+        this.validate();
+    }
+
+    get name(): string{
+        return this._name;
+    }
+
+    changePrice(price: number): void{
+        this._price = price;
+        this.validate();
+    }
+
+    get price(): number{
+        return this._price;
+    }
+
     validate(): boolean{
         if(this._id.length === 0){
             throw new Error("id is required");
+        }
+        
+        if(this._name.length === 0){
+            throw new Error("name is required");
+        }
+
+        if(this._price < 0){ 
+            throw new Error("price must be greater than zero");
         }
         return true;
     }
