967603053834c53042481e34746775c380bff36a
leofontolan <leofontolan@gmail.com>
2022-05-31 20:44:04 -0300

    Implementando findAll

--- src/infrasctructure/repository/product.repository.ts
+++ src/infrasctructure/repository/product.repository.ts
@@ -27,10 +27,21 @@ export default class ProductRepository implements ProductRepositoryInterface{
     }
     
     async find(id: string): Promise<Product> {
-        throw new Error("Method not implemented.");
+        const productModel = await ProductModel.findOne({ where: { id } });
+
+        return new Product(
+            productModel.id,
+            productModel.name,
+            productModel.price
+        );
     }
     
     async findAll(): Promise<Product[]> {
-        throw new Error("Method not implemented.");
+        const productModels = await ProductModel.findAll();
+        return productModels.map(productModel => new Product(
+            productModel.id,
+            productModel.name,
+            productModel.price
+        ));
     }
 }
