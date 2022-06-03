fca9e0996d7a70618636cbd5bc59ad330ac11198
leofontolan <leofontolan@gmail.com>
2022-06-03 20:01:50 -0300

    Criando Tabelas de Order

---
+++ src/infrasctructure/db/sequelize/model/order.model.ts
@@ -0,0 +1,29 @@
+import {
+    Table,
+    Model,
+    PrimaryKey,
+    Column,
+    ForeignKey,
+    BelongsTo
+  } from "sequelize-typescript";
+import CustomerModel from "./customer.model";
+
+  @Table({
+    tableName: "orders",
+    timestamps: false,
+  })
+  export default class OrderModel extends Model {
+    @PrimaryKey
+    @Column
+    declare id: string;
+  
+    @ForeignKey(() => CustomerModel)
+    @Column({allowNull: false})
+    declare customer_id: string;
+
+    @BelongsTo(() => CustomerModel)
+    declare customer:CustomerModel;
+
+    @Column({allowNull: false})
+    declare total: number;
+  }
